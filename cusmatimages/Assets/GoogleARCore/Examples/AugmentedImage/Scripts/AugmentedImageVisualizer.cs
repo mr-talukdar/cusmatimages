e04ca0abcd72e208edfd07f50ad5096a325f8fb5
Rahul Talukdar <1875020@kiit.ac.in>
2020-08-25 15:55:51 +0530

    LeanTouch

--- cusmatimages/Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
+++ cusmatimages/Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
@@ -26,6 +26,7 @@ namespace GoogleARCore.Examples.AugmentedImage
     using GoogleARCore;
     using GoogleARCoreInternal;
     using UnityEngine;
+    using UnityEngine.UIElements;
 
     /// <summary>
     /// Uses 4 frame corner objects to visualize an AugmentedImage.
@@ -40,38 +41,41 @@ namespace GoogleARCore.Examples.AugmentedImage
         /// <summary>
         /// A model for the lower left corner of the frame to place when an image is detected.
         /// </summary>
-        public GameObject FrameLowerLeft;
+        //public GameObject FrameLowerLeft;
 
         /// <summary>
         /// A model for the lower right corner of the frame to place when an image is detected.
         /// </summary>
-        public GameObject FrameLowerRight;
+        //public GameObject FrameLowerRight;
 
         /// <summary>
         /// A model for the upper left corner of the frame to place when an image is detected.
         /// </summary>
-        public GameObject FrameUpperLeft;
+        // GameObject FrameUpperLeft;
 
         /// <summary>
         /// A model for the upper right corner of the frame to place when an image is detected.
         /// </summary>
-        public GameObject FrameUpperRight;
+        //public GameObject FrameUpperRight;
 
         /// <summary>
         /// The Unity Update method.
         /// </summary>
+        /// 
+        public GameObject engine;
         public void Update()
         {
             if (Image == null || Image.TrackingState != TrackingState.Tracking)
             {
-                FrameLowerLeft.SetActive(false);
+                /*FrameLowerLeft.SetActive(false);
                 FrameLowerRight.SetActive(false);
                 FrameUpperLeft.SetActive(false);
-                FrameUpperRight.SetActive(false);
+                FrameUpperRight.SetActive(false);*/
+                engine.SetActive(false);
                 return;
             }
 
-            float halfWidth = Image.ExtentX / 2;
+            /*float halfWidth = Image.ExtentX / 2;
             float halfHeight = Image.ExtentZ / 2;
             FrameLowerLeft.transform.localPosition =
                 (halfWidth * Vector3.left) + (halfHeight * Vector3.back);
@@ -80,12 +84,17 @@ namespace GoogleARCore.Examples.AugmentedImage
             FrameUpperLeft.transform.localPosition =
                 (halfWidth * Vector3.left) + (halfHeight * Vector3.forward);
             FrameUpperRight.transform.localPosition =
-                (halfWidth * Vector3.right) + (halfHeight * Vector3.forward);
+                (halfWidth * Vector3.right) + (halfHeight * Vector3.forward);*/
 
-            FrameLowerLeft.SetActive(true);
+            Pose centerPose = Image.CenterPose;
+            Vector3 center = centerPose.position;
+            engine.transform.localPosition = center;
+
+            /*FrameLowerLeft.SetActive(true);
             FrameLowerRight.SetActive(true);
             FrameUpperLeft.SetActive(true);
-            FrameUpperRight.SetActive(true);
+            FrameUpperRight.SetActive(true);*/
+            engine.SetActive(true);
         }
     }
 }
